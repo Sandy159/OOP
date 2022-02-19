a2bd6445da54f2f7396a9261f2fedba78197322a
Besedina <77335821+Sandy159@users.noreply.github.com>
2022-02-19 21:28:00 +0700

    Добавлен итератор для возможности реализовать range_based_for для тритов

--- Lab1/tritset.h
+++ Lab1/tritset.h
@@ -17,46 +17,64 @@ Trit operator|(Trit a, Trit b);
 Trit operator~(Trit a);
 
 class Tritset {
- private:
-  vector<unsigned int> arr;
-  size_t count_False = 0;
-  size_t count_True = 0;
-  int beginning_size;
-  int current_size;
+private:
+    vector<unsigned int> arr;
+    size_t count_False = 0;
+    size_t count_True = 0;
+    int beginning_size;
+    int current_size;
 
-  void resize_vector(int count_trit);
-  void change_trit_by_index(int index, Trit trit);
-  int get_last_set_int_index();
-  int get_last_set_trit_index();
+    void resize_vector(int count_trit);
+    void change_trit_by_index(int index, Trit trit);
+    int get_last_set_int_index() const;
+    int get_last_set_trit_index() const;
 
-  friend class Proxy_trit;
-  class Proxy_trit {
-    vector<unsigned int>& arr_;
-    int index_;
-    Tritset& tritset_;
-    Trit value;
+    friend class Proxy_trit;
+    class Proxy_trit {
+        vector<unsigned int>& arr_;
+        int index_;
+        Tritset& tritset_;
+        Trit value;
 
-    Trit get_value();
-    void change_count_True_and_False(Trit trit);
+        Trit get_value();
+        void change_count_True_and_False(Trit trit);
 
-   public:
-    Proxy_trit(int index, vector<unsigned int>& arr, Tritset& tritset);
-    Trit operator=(Trit trit);
-    Trit operator=(Proxy_trit p_trit);
-    operator Trit();
-  };
+    public:
+        Proxy_trit(int index, vector<unsigned int>& arr, Tritset& tritset);
+        Proxy_trit(int index, const vector<unsigned int>& arr, const Tritset& tritset);
+        Trit operator=(Trit trit);
+        Trit operator=(Proxy_trit p_trit);
+        operator Trit();
+    };
 
- public:
-  Tritset(int count_trit);
-  int get_size_in_trit();
-  static int convert_int_index_to_trit_index(int int_index);
-  static int convert_trit_index_to_int_index(int trit_index);
-  Proxy_trit operator[](int index);
-  void shrink();
-  size_t cardinality(Trit value);
-  unordered_map<Trit, size_t> cardinality();
-  void trim(size_t lastIndex);
-  size_t length();
+    friend class Iterator;
+    class Iterator {
+    private:
+        const Tritset& tritset_;
+        int index_;
+    public:
+        Iterator(const int index, const Tritset& tritset);
+        Iterator operator++();
+        bool operator==(Iterator& iter);
+        bool operator!=(Iterator& iter);
+        Proxy_trit operator*();
+    };
+
+public:
+    Tritset(int count_trit);
+    int get_size_in_trit() const;
+    static int convert_int_index_to_trit_index(int int_index);
+    static int convert_trit_index_to_int_index(int trit_index);
+    Proxy_trit operator[](int index) const;
+
+    Iterator begin() const;
+    Iterator end() const;
+
+    void shrink();
+    size_t cardinality(Trit value);
+    unordered_map<Trit, size_t> cardinality();
+    void trim(size_t lastIndex);
+    size_t length() const;
 };
 
 #endif  //  TRITSET_H_
